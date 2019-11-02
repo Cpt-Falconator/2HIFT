f3667c96769d7268c113d4837961d5323255851b
Lewis Thomson <1601828@uad.ac.uk>
2019-11-02 13:01:44 +0000

    title colour swap

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameLogic : MonoBehaviour
+{
+
+    public Image topPanel, bottomPanel;
+    public float shiftTimer;
+    private float timer;
+    private int state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        topPanel.color = Color.white;
+        bottomPanel.color = Color.black;
+        timer = 0;
+        state = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(timer >= shiftTimer)
+        {
+            switch(state)
+            {
+                case 0:
+                    state = 1;
+                    topPanel.color = Color.black;
+                    bottomPanel.color = Color.white;
+                    timer = 0;
+                    break;
+
+                case 1:
+                    state = 0;
+                    topPanel.color = Color.white;
+                    bottomPanel.color = Color.black;
+                    timer = 0;
+                    break;
+
+            }
+            
+        }
+        timer += 1 * Time.deltaTime;
+    }
+}
